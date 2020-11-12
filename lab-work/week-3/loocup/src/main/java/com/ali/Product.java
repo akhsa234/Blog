a5a09e608d631efa54f000b21c356e73b5156cc0
alipiry77 <pirghazani.ali@gmail.com>
2020-11-12 12:42:14 +0330

    new loocup

---
+++ lab-work/week-3/loocup/src/main/java/com/ali/Product.java
@@ -0,0 +1,30 @@
+package com.ali;
+
+public class Product {
+    private String name ;
+    private long price ;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
